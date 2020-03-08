6985ae469604fdeb73ca76fb09ff99ad9c50231b
Isitar <luescherpascal@gmail.com>
2020-03-08 18:02:21 +0100

    enabled more bulma modules, progress bar when loading plant detail

--- src/views/PlantDetail.vue
+++ src/views/PlantDetail.vue
@@ -1,8 +1,16 @@
 <template>
     <main>
         <section class="section">
-            <div class="container">
+            <div class="container" v-if="plant">
                 <h1 class="title">{{plant.name}}</h1>
+                <div class="tile is-ancestor">
+                    <div class="tile">
+                        <p>Species: <strong class="has-text-primary">{{plant.plantSpeciesId}}</strong></p>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <progress class="progress is-primary" max="100" v-if="!plant"></progress>
             </div>
         </section>
     </main>
@@ -10,11 +18,16 @@
 
 <script lang="ts">
     import Vue from 'vue';
+    import {Plant} from "@/models/Plant";
 
     export default Vue.extend({
         name: 'PlantDetail',
         computed: {
-            plant() {
+            plant(): Plant {
+                const plant = this.$store.getters.getPlantById(this.$route.params.id);
+                if (undefined === plant) {
+                    this.$store.dispatch('loadPlants');
+                }
                 return this.$store.getters.getPlantById(this.$route.params.id);
             }
         }
